a3c199e40b53a397d3a21e2f3e6b302fc3fc125c
EmperorMoose <pmatt42@gmail.com>
2017-05-29 22:48:26 -0700

    Setting up the habit activity from listview

--- app/src/main/java/com/example/emperormoose/habitapp/MainActivity.java
+++ app/src/main/java/com/example/emperormoose/habitapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.emperormoose.habitapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,9 +18,13 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 import static android.R.interpolator.linear;
+import static com.example.emperormoose.habitapp.R.id.text;
+import static com.example.emperormoose.habitapp.R.layout.activity_main;
+import static com.example.emperormoose.habitapp.R.layout.activity_habit;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variables for this class
     private ListView mListView;
     private EditText mEditText;
     private Button mButton;
@@ -27,37 +33,45 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Standard start of android stuff
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
 
+        //Variables for views and the button listener
         mListView = (ListView) findViewById(R.id.mainListView);
         mEditText = (EditText) findViewById(R.id.editText);
         mButton = (Button) findViewById(R.id.button);
         mButton.setOnClickListener(onClick());
 
+        //Sets the default text for the textview
         TextView textView = new TextView(this);
         textView.setText("New text");
-
+        //The adapter handles the listview
         listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, habitList);
+        mListView.setAdapter(listAdapter);
+
+        //TODO fix the errors in this block. activity.class is unknown, despite being defined. Figure out how intent works,
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent mIntent = new Intent(view.getContext(), activity_habit.class);
+                mIntent.putExtra(text);
+                startActivity(mIntent);
+            }
+        });
     }
 
+    //listener for the button
     private View.OnClickListener onClick() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Get the text from the edit text
                 String text =  mEditText.getText().toString();
+                //add the text to the adapter
                 listAdapter.add(text);
-                mListView.setAdapter(listAdapter);
-                //mListView.addView(createNewTextView(mEditText.getText().toString()));
+                //place the adapter in the view
             }
         };
-    }
-
-    private TextView createNewTextView(String text) {
-        final ActionBar.LayoutParams lparams = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
-        final TextView textView = new TextView(this);
-        textView.setLayoutParams(lparams);
-        textView.setText("New text: " + text);
-        return textView;
     }
 }
